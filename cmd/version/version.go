627aedfda3aba5a5d3883dcc5d0e27d18c1f169b
Ondrej Sika <ondrej@ondrejsika.com>
2021-07-30 00:01:05 +0200

    feat: Create backbone & version command

---
+++ cmd/version/version.go
@@ -0,0 +1,19 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var version string = "v0.1.0-dev"
+var gitShortSha string = "dev"
+
+var VersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints version",
+	Args:  cobra.NoArgs,
+	Run: func(c *cobra.Command, args []string) {
+		fmt.Printf("%s %s\n", version, gitShortSha)
+	},
+}
